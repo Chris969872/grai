4a67dde9de41d6a567c0fc2a3e2e9578f90236de
Chris Atkinson <chris.atkinson.ca@gmail.com>
2013-10-11 12:42:27 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Grai::Application.config.secret_token = '0a79c8c6e53479fc609fc150f615ac6b3c5c3f3c467767fe832d212e0a0cbe676ece4df1ce58de20563b4caf517e979ffc7d269eb3449114a34ee079364b39de'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Grai::Application.config.session_store :cookie_store, key: '_grai_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Grai::Application.config.session_store :active_record_store
